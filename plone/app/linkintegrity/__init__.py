977860e5037c5abca6a801adbc45a8100b7e7ba4
Andreas Zeidler <witsch@plone.org>
2006-12-24 11:15:14 +0000

    allowance of the import of the exception should happen in `CMFPlone` (and [http://dev.plone.org/plone/changeset/11686#file0 does]) as it would otherwise cause an error without `plone.app.linkintegrity`
    svn path=/plone.app.linkintegrity/trunk/; revision=11688

--- plone/app/linkintegrity/__init__.py
+++ plone/app/linkintegrity/__init__.py
@@ -1,4 +1,3 @@
-from AccessControl import allow_module, allow_class
 from os.path import abspath, dirname, basename
 
 packageHome = abspath(dirname(__file__))
@@ -12,9 +11,4 @@ def initialize(context):
     # install the wrapper around zpublisher_exception_hook
     from monkey import installExceptionHook
     installExceptionHook()
-    
-    # allow access to the exception in the folder_delete script
-    from OFS.ObjectManager import BeforeDeleteException
-    allow_module('OFS.ObjectManager')
-    allow_class(BeforeDeleteException)
 
