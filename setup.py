25b1d396276115ccf7214e702c0ebf644e6be634
Hanno Schlichting <hanno@hannosch.eu>
2007-01-21 20:19:22 +0000

    Ignore useless folders and include setuptools into the dependencies.
    svn path=/plone.app.linkintegrity/trunk/; revision=12016

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(name='plone.app.linkintegrity',
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          # -*- Extra requirements: -*-
+        'setuptools',
       ],
       entry_points="""
       # -*- Entry points: -*-
