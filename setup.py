5b9a9a261cc7170b34338895f26820645c4635be
Andreas Zeidler <witsch@plone.org>
2007-03-01 23:50:59 +0000

    bumped version number for the beta1 & updated history
    svn path=/plone.app.linkintegrity/trunk/; revision=13366

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name = 'plone.app.linkintegrity',
-      version = '1.0a3',
+      version = '1.0b1',
       description = 'Manage link integrity in Plone.',
       keywords = 'link integrity plone',
       author = 'Plone Foundation',
