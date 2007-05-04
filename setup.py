5b8eb43cea55157a2d4a1d8454def8db7e1baf95
Andreas Zeidler <witsch@plone.org>
2007-05-04 09:27:07 +0000

    preparation for the beta3 ("we don't want mac cruft") release
    svn path=/plone.app.linkintegrity/trunk/; revision=14798

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name = 'plone.app.linkintegrity',
-      version = '1.0b2',
+      version = '1.0b3',
       description = 'Manage link integrity in Plone.',
       keywords = 'link integrity plone',
       author = 'Plone Foundation',
