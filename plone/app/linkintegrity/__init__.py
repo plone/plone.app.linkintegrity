5f9488524126b90b160a2c32af15d8b1829d444d
David Glick <davidglick@groundwire.org>
2009-07-01 07:40:08 +0000

    don't install the exception hook in Zope >=2.12
    svn path=/plone.app.linkintegrity/trunk/; revision=27757

--- plone/app/linkintegrity/__init__.py
+++ plone/app/linkintegrity/__init__.py
@@ -1,6 +1,20 @@
+# test for Zope 2.12 -- the first eggified Zope
+import pkg_resources
+try:
+    dist=pkg_resources.get_distribution('Zope2')
+    HAS_ZOPE_212 = True
+except pkg_resources.DistributionNotFound:
+    HAS_ZOPE_212 = False
+
 def initialize(context):
 
-    # install the wrapper around zpublisher_exception_hook
-    from monkey import installExceptionHook
-    installExceptionHook()
+    # side effect import to patch the response's retry method
+    import monkey
 
+    # If < Zope 2.12, install the wrapper around zpublisher_exception_hook.
+    # The patch is not needed or wanted in Zope 2.12, which already includes
+    # the error view lookup in its ZPublisherExceptionHook, and which sometimes
+    # expects the error message to be returned instead of raised depending on 
+    # the response's handle_errors attribute.
+    if not HAS_ZOPE_212:
+        monkey.installExceptionHook()
