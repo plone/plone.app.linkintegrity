b03c38293035643c6a09a84d57577b75ddc9580b
Andreas Zeidler <witsch@plone.org>
2009-08-07 17:12:23 +0000

    update for r28458: don't disable the error handling for zope < 2.12
    svn path=/plone.app.linkintegrity/trunk/; revision=28501

--- plone/app/linkintegrity/tests/testDocTests.py
+++ plone/app/linkintegrity/tests/testDocTests.py
@@ -1,6 +1,6 @@
 # setup tests with all doctests found in docs/
 
-from plone.app.linkintegrity import docs
+from plone.app.linkintegrity import docs, HAS_ZOPE_212
 from plone.app.linkintegrity.tests import layer
 from plone.app.linkintegrity.parser import extractLinks
 from Testing.ZopeTestCase import FunctionalDocFileSuite
@@ -38,7 +38,7 @@ class LinkIntegrityFunctionalTestCase(PloneTestCase.FunctionalTestCase):
     def getBrowser(self, loggedIn=False):
         """ instantiate and return a testbrowser for convenience """
         browser = Browser()
-        browser.handleErrors = False
+        browser.handleErrors = not HAS_ZOPE_212
         if loggedIn:
             user = PloneTestCase.default_user
             pwd = PloneTestCase.default_password
