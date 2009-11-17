2f54020895adb2ef4c707ec1f670d3b195f5e53b
Hanno Schlichting <hanno@hannosch.eu>
2009-11-17 17:37:28 +0000

    Maintain changelog and standardize metadata
    svn path=/plone.app.linkintegrity/trunk/; revision=31775

--- setup.py
+++ setup.py
@@ -1,21 +1,16 @@
 from setuptools import setup, find_packages
-from os.path import join
 
-name = 'plone.app.linkintegrity'
-path = name.split('.') + ['version.txt']
-version = open(join(*path)).read().strip()
-readme = open("README.txt").read()
-history = open(join('docs', 'HISTORY.txt')).read().replace(name + ' - ', '')
+version = '1.3a1'
 
-setup(name = name,
+setup(name = 'plone.app.linkintegrity',
       version = version,
       description = 'Manage link integrity in Plone.',
-      long_description = readme[readme.find('\n\n'):] + '\n' + history,
+      long_description = open("README.txt").read() + '\n' +
+                         open('CHANGES.txt').read(),
       keywords = 'link integrity plone',
       author = 'Plone Foundation',
       author_email = 'plone-developers@lists.sourceforge.net',
-      url = 'http://svn.plone.org/svn/plone/plone.app.linkintegrity/',
-      download_url = 'http://cheeseshop.python.org/pypi/plone.app.linkintegrity/',
+      url = 'http://pypi.python.org/pypi/plone.app.linkintegrity',
       license = 'GPL',
       packages = find_packages(),
       namespace_packages = ['plone', 'plone.app'],
@@ -28,14 +23,11 @@ setup(name = name,
         'Environment :: Web Environment',
         'Framework :: Plone',
         'Framework :: Zope2',
-        'Framework :: Zope3',
         'Intended Audience :: Other Audience',
         'Intended Audience :: System Administrators',
         'License :: OSI Approved :: GNU General Public License (GPL)',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Internet :: WWW/HTTP :: Site Management :: Link Checking',
       ],
 )
-
