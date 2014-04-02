4a5f1b1b145a1e7cf173052996f88ab9232b4832
Patrick Gerken <do3cc@patrick-gerken.de>
2014-04-02 17:47:09 +0200

    Bad monkey 2

--- plone/app/linkintegrity/monkey2.py
+++ plone/app/linkintegrity/monkey2.py
@@ -1,9 +1,11 @@
 
 import OFS.ObjectManager
+
+original = OFS.ObjectManager.ObjectManager.manage_delObjects
 # FIXME: Plone needs an api in core
 def manage_delObjects(self, ids=None, REQUEST=None):
     if REQUEST is not None and not isinstance(ids, basestring):
         REQUEST.set('link_integrity_events_to_expect', len(ids))
-    return OFS.ObjectManager.ObjectManager.manage_delObjects(self, ids, REQUEST)
+    return original(self, ids, REQUEST)
 
 OFS.ObjectManager.ObjectManager.manage_delObjects = manage_delObjects
