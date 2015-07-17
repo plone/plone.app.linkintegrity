7de4f7a1826e4aa3d8d17c0aee34640ab667551f
Ramon Navarro Bosch <ramon.nb@gmail.com>
2015-07-17 09:56:56 +0200

    More cleanup and archetypes

--- plone/app/linkintegrity/__init__.py
+++ plone/app/linkintegrity/__init__.py
@@ -1,10 +1,1 @@
-try:
-    # this import not used except as a test for Zope 2.12+
-    from Products.Five.bbb import AcquisitionBBB
-    AcquisitionBBB      # make pyflakes happy
-    HAS_ZOPE_212 = True
-except ImportError:
-    HAS_ZOPE_212 = False
-
-import pkg_resources
-
+# -*- encoding: utf-8 -*-
--- plone/app/linkintegrity/handlers.py
+++ plone/app/linkintegrity/handlers.py
@@ -140,7 +140,8 @@ def modifiedArchetype(obj, event):
                 value = field.get(obj)
             links = extractLinks(value, encoding)
             refs |= getObjectsFromLinks(obj, links)
-    updateReferences(obj, referencedRelationship, refs)
+    for ref in refs:
+        _setRelation(obj, referencedRelationship, ref)
 
 
 def modifiedDexterity(obj, event):
@@ -173,71 +174,71 @@ def modifiedDexterity(obj, event):
         _setRelation(obj, referencedRelationship, ref)
 
 
-def referenceRemoved(obj, event):
-    """ store information about the removed link integrity reference """
-    assert IReference.providedBy(obj)
-    assert obj is event.object          # just making sure...
-    if not obj.relationship == referencedRelationship:
-        return                          # skip for other removed references
-    # if the object the event was fired on doesn't have a `REQUEST` attribute
-    # we can safely assume no direct user action was involved and therefore
-    # never raise a link integrity exception...
-    request = aq_get(obj, 'REQUEST', None)
-    if not request:
-        return
-    storage = ILinkIntegrityInfo(request)
-    source = obj.getSourceObject()
-    if not IBaseObject.providedBy(source) and hasattr(source, 'context'):
-        source = source.context
-    target = obj.getTargetObject()
-    if not IBaseObject.providedBy(target) and hasattr(target, 'context'):
-        target = target.context
-    if source is not None and target is not None:
-        storage.addBreach(source, target)
+# def referenceRemoved(obj, event):
+#     """ store information about the removed link integrity reference """
+#     assert IReference.providedBy(obj)
+#     assert obj is event.object          # just making sure...
+#     if not obj.relationship == referencedRelationship:
+#         return                          # skip for other removed references
+#     # if the object the event was fired on doesn't have a `REQUEST` attribute
+#     # we can safely assume no direct user action was involved and therefore
+#     # never raise a link integrity exception...
+#     request = aq_get(obj, 'REQUEST', None)
+#     if not request:
+#         return
+#     storage = ILinkIntegrityInfo(request)
+#     source = obj.getSourceObject()
+#     if not IBaseObject.providedBy(source) and hasattr(source, 'context'):
+#         source = source.context
+#     target = obj.getTargetObject()
+#     if not IBaseObject.providedBy(target) and hasattr(target, 'context'):
+#         target = target.context
+#     if source is not None and target is not None:
+#         storage.addBreach(source, target)
 
 
-def referencedObjectRemoved(obj, event):
-    """ check if the removal was already confirmed or redirect to the form """
-    # if the object the event was fired on doesn't have a `REQUEST` attribute
-    # we can safely assume no direct user action was involved and therefore
-    # never raise a link integrity exception...
-    request = aq_get(obj, 'REQUEST', None)
-    if not request:
-        return
-    info = ILinkIntegrityInfo(request)
-    # first we check if link integrity checking was enabled
-    if not info.integrityCheckingEnabled():
-        return
+# def referencedObjectRemoved(obj, event):
+#     """ check if the removal was already confirmed or redirect to the form """
+#     # if the object the event was fired on doesn't have a `REQUEST` attribute
+#     # we can safely assume no direct user action was involved and therefore
+#     # never raise a link integrity exception...
+#     request = aq_get(obj, 'REQUEST', None)
+#     if not request:
+#         return
+#     info = ILinkIntegrityInfo(request)
+#     # first we check if link integrity checking was enabled
+#     if not info.integrityCheckingEnabled():
+#         return
 
-    # since the event gets called for every subobject before it's
-    # called for the item deleted directly via _delObject (event.object)
-    # itself, but we do not want to present the user with a confirmation
-    # form for every (referred) subobject, so we remember and skip them...
-    info.addDeletedItem(obj)
-    if obj is not event.object:
-        return
+#     # since the event gets called for every subobject before it's
+#     # called for the item deleted directly via _delObject (event.object)
+#     # itself, but we do not want to present the user with a confirmation
+#     # form for every (referred) subobject, so we remember and skip them...
+#     info.addDeletedItem(obj)
+#     if obj is not event.object:
+#         return
 
-    # if the number of expected events has been stored to help us prevent
-    # multiple forms (i.e. in folder_delete), we wait for the next event
-    # if we know there will be another...
-    if info.moreEventsToExpect():
-        return
+#     # if the number of expected events has been stored to help us prevent
+#     # multiple forms (i.e. in folder_delete), we wait for the next event
+#     # if we know there will be another...
+#     if info.moreEventsToExpect():
+#         return
 
-    # at this point all subobjects have been removed already, so all
-    # link integrity breaches caused by that have been collected as well;
-    # if there aren't any (after things have been cleaned up),
-    # we keep lurking in the shadows...
-    if not info.getIntegrityBreaches():
-        return
+#     # at this point all subobjects have been removed already, so all
+#     # link integrity breaches caused by that have been collected as well;
+#     # if there aren't any (after things have been cleaned up),
+#     # we keep lurking in the shadows...
+#     if not info.getIntegrityBreaches():
+#         return
 
-    # if the user has confirmed to remove the currently handled item in a
-    # previous confirmation form we won't need it anymore this time around...
-    if info.isConfirmedItem(obj):
-        return
+#     # if the user has confirmed to remove the currently handled item in a
+#     # previous confirmation form we won't need it anymore this time around...
+#     if info.isConfirmedItem(obj):
+#         return
 
-    # otherwise we raise an exception and pass the object that is supposed
-    # to be removed as the exception value so we can use it as the context
-    # for the view triggered by the exception;  this is needed since the
-    # view is an adapter for the exception and a request, so it gets the
-    # exception object as the context, which is not very useful...
-    raise LinkIntegrityNotificationException(obj)
+#     # otherwise we raise an exception and pass the object that is supposed
+#     # to be removed as the exception value so we can use it as the context
+#     # for the view triggered by the exception;  this is needed since the
+#     # view is an adapter for the exception and a request, so it gets the
+#     # exception object as the context, which is not very useful...
+#     raise LinkIntegrityNotificationException(obj)
