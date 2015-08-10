0d9d4ff3fb295f8f3003c3bd0e015b27f70f9100
Philip Bauer <bauer@starzel.de>
2015-08-10 20:47:06 +0200

    rewrite check for breaches: - ignore breaches from children of deleted item - fix showing info about targets in delete_confirmation - allow to check multiple items and account for circular references

--- plone/app/linkintegrity/browser/info.py
+++ plone/app/linkintegrity/browser/info.py
@@ -5,14 +5,107 @@ from Products.CMFCore.permissions import AccessContentsInformation
 from Products.CMFCore.utils import getToolByName, _checkPermission
 from Products.Five import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from collections import defaultdict
 from plone.app.linkintegrity.utils import getIncomingLinks
 from plone.app.linkintegrity.utils import linkintegrity_enabled
+from plone.uuid.interfaces import IUUID
 from zope.i18n import translate
 
 
 class DeleteConfirmationInfo(BrowserView):
 
     template = ViewPageTemplateFile('delete_confirmation_info.pt')
+
+    def __call__(self):
+        self.linkintegrity_enabled = linkintegrity_enabled()
+        return self.template()
+
+    def get_breaches_for_items(self, items):
+        """Check for breaches when deleting multiple items at once.
+        Breaches originating from items that will also be deleted are dropped.
+        """
+        catalog = getToolByName(self.context, 'portal_catalog')
+        results = []
+        uids_to_delete = []
+        for obj in items:
+            obj_path = '/'.join(obj.getPhysicalPath())
+            brains_to_delete = catalog(path={'query': obj_path})
+            uids_to_delete.extend([i.UID for i in brains_to_delete])
+            for breach in self.get_breaches(obj):
+                add_breach = False
+                for source in breach['sources']:
+                    if source['uid'] not in uids_to_delete:
+                        add_breach = True
+                if add_breach:
+                    results.append(breach)
+
+        # cleanup (some breaches where added before it was clear
+        # that the source will be deleted too)
+        for result in results:
+            for source in result['sources']:
+                if source['uid'] in uids_to_delete:
+                    result['sources'].remove(source)
+                    if not result['sources']:
+                        results.remove(result)
+        return results
+
+    def get_breaches(self, obj=None):
+        """Check one object and it's children for breaches.
+        """
+        if obj is None:
+            obj = self.context
+        results = []
+        catalog = getToolByName(obj, 'portal_catalog')
+        obj_path = '/'.join(obj.getPhysicalPath())
+
+        breaches = self.check_object(obj)
+        if breaches:
+            results.append(breaches)
+
+        if IFolder.providedBy(obj):
+            brains = catalog(path={'query': obj_path})
+            for brain in brains:
+                child = brain.getObject()
+                if child == obj:
+                    continue
+                breaches = self.check_object(obj=child, excluded_path=obj_path)
+                if breaches:
+                    results.append(breaches)
+        self.breaches = results
+        return results
+
+    def check_object(self, obj, excluded_path=None):
+        """Check one object for breaches.
+        If breaches origin from excluded_path ignore them
+        """
+        if excluded_path is None:
+            excluded_path = '/'.join(obj.getPhysicalPath())
+        breaches = defaultdict(list)
+        direct_links = getIncomingLinks(obj)
+        has_breaches = False
+        for direct_link in direct_links:
+            source_path = direct_link.from_path
+            if not source_path or source_path.startswith(excluded_path):
+                # broken or child of item that is to be deleted
+                continue
+            source = direct_link.from_object
+            breaches['sources'].append({
+                'uid': IUUID(source),
+                'title': source.Title(),
+                'url': source.absolute_url(),
+                'accessible': self.isAccessible(source),
+            })
+            has_breaches = True
+        if has_breaches:
+            breaches['target'] = {
+                'uid': IUUID(obj),
+                'title': obj.Title(),
+                'url': obj.absolute_url(),
+                'portal_type': obj.portal_type,
+                'type_title': self.getPortalTypeTitle(obj),
+            }
+        if has_breaches:
+            return breaches
 
     def getPortalTypeTitle(self, obj):
         # Get the portal type title of the object.
@@ -28,47 +121,3 @@ class DeleteConfirmationInfo(BrowserView):
 
     def isAccessible(self, obj):
         return _checkPermission(AccessContentsInformation, obj)
-
-    def shallowCheckObject(self, obj):
-        result = []
-        for element in getIncomingLinks(obj):
-            obj = element.from_object
-            if obj:
-                result.append(element.from_object)
-
-        if len(result):
-            return {
-                'title': obj.Title(),
-                'url': obj.absolute_url(),
-                'sources': result,
-                'type': obj.getPortalTypeName(),
-                'type_title': self.getPortalTypeTitle(obj)
-            }
-
-    def checkObject(self, obj):
-        if not hasattr(self, 'breaches'):
-            self.breaches = []
-        check = self.shallowCheckObject(obj)
-        if check:
-            self.breaches.append(check)
-
-        if IFolder.providedBy(obj):
-            # now check if folder and go through children
-            # looking for links....
-            # Unfortunately, there doesn't seem to be a better,
-            # less expensive way to do this. This operation could
-            # potentially cost a lot of cycles...
-            catalog = getToolByName(self.context, 'portal_catalog')
-            folder_path = '/'.join(obj.getPhysicalPath())
-            for brain in catalog(path={'query': folder_path}):
-                ob = brain.getObject()
-                check = self.shallowCheckObject(ob)
-                if check:
-                    self.breaches.append(check)
-
-    def __call__(self, skip_context=False):
-        if not linkintegrity_enabled():
-            self.breaches = []
-        elif not skip_context:
-            self.checkObject(self.context)
-        return self.template()
--- plone/app/linkintegrity/tests/test_circular.py
+++ plone/app/linkintegrity/tests/test_circular.py
@@ -5,8 +5,6 @@ from plone.app.linkintegrity.utils import hasIncomingLinks
 from plone.app.linkintegrity.utils import getOutgoingLinks
 from plone.app.linkintegrity.browser.info import DeleteConfirmationInfo
 
-import transaction
-
 
 class CircularReferencesTestCase:
 
@@ -23,48 +21,34 @@ class CircularReferencesTestCase:
         self.assertTrue(hasIncomingLinks(doc1))
         self.assertTrue(hasIncomingLinks(doc2))
         self.assertTrue(hasIncomingLinks(doc3))
-        transaction.abort()
-
-        self.portal.manage_delObjects(
-            ['doc1', 'doc2', 'doc3'], self.request)
-        self.assertNotIn('doc1', self.portal)
-        self.assertNotIn('doc2', self.portal)
-        self.assertNotIn('doc3', self.portal)
-        self.assertRaises(KeyError, hasIncomingLinks, [doc1])
-        self.assertRaises(KeyError, hasIncomingLinks, [doc2])
-        self.assertRaises(KeyError, hasIncomingLinks, [doc2])
 
     def test_circular_reference_subfolder_deletion(self):
         doc1 = self.portal.doc1
         doc2 = self.portal.doc2
+        doc3 = self.portal.doc3
         doc4 = self.portal.folder1.doc4
+        folder1 = self.portal.folder1
 
-        # This tests the behaviour when removing three object
-        # referencing each other in a circle.  This situation cannot be
-        # resolved completely, since the removal events are fired
-        # separately.  However, the circle gets "broken up" when
-        # confirming the removal of the first object, and no further
-        # confirmation form are necessary:
+        # This tests the behaviour when removing objects
+        # referencing each other in a circle.
         self._set_text(doc1, '<a href="doc2">documents...</a>')
-        self._set_text(doc2, '<a href="folder1/doc4">linking...</a>')
+        self._set_text(doc2, '<a href="doc3">go round...</a>')
+        self._set_text(doc3, '<a href="folder1/doc4">and round.</a>')
         self._set_text(doc4, '<a href="../doc1">in circles.</a>')
 
-        self.assertEqual([r.to_object for r in getOutgoingLinks(doc1)], [doc2, ])
-        self.assertEqual([r.to_object for r in getOutgoingLinks(doc2)], [doc4, ])
-        self.assertEqual([r.to_object for r in getOutgoingLinks(doc4)], [doc1, ])
+        self.assertEqual([r.to_object for r in getOutgoingLinks(doc1)], [doc2])
+        self.assertEqual([r.to_object for r in getOutgoingLinks(doc2)], [doc3])
+        self.assertEqual([r.to_object for r in getOutgoingLinks(doc3)], [doc4])
+        self.assertEqual([r.to_object for r in getOutgoingLinks(doc4)], [doc1])
 
         view = DeleteConfirmationInfo(self.portal, self.request)
-        view.checkObject(doc1)
-        view.checkObject(doc2)
-        self.assertEqual(len(view.breaches), 2)
-        transaction.abort()
+        self.assertEqual(len(view.get_breaches_for_items([folder1])), 1)
 
-        self.portal.manage_delObjects(
-            ['doc1', 'doc2', 'folder1', ], self.request)
+        self.assertEqual(
+            len(view.get_breaches_for_items([doc1, doc2, doc3, folder1])), 0)
+        self.assertEqual(
+            len(view.get_breaches_for_items([doc2, folder1])), 2)
 
-        self.assertNotIn('doc1', self.portal)
-        self.assertNotIn('doc2', self.portal)
-        self.assertNotIn('folder1', self.portal)
 
 
 class CircularReferencesDXTestCase(DXBaseTestCase, CircularReferencesTestCase):
