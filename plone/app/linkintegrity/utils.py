1ffe718117050509bbf06bfdcc1e2cdb5f83556b
Philip Bauer <bauer@starzel.de>
2015-08-10 20:44:31 +0200

    allow to pass only a intid to check for links

--- plone/app/linkintegrity/utils.py
+++ plone/app/linkintegrity/utils.py
@@ -7,44 +7,54 @@ from zope.component import getUtility
 from zope.intid.interfaces import IIntIds
 
 
-def getIncomingLinks(obj):
+def getIncomingLinks(obj=None, intid=None):
     """Return a generator of incoming relations created using
     plone.app.linkintegrity (Links in Richtext-Fields).
     """
     catalog = getUtility(ICatalog)
-    intids = getUtility(IIntIds)
-    return catalog.findRelations({
-        'to_id': intids.getId(obj),
-        'from_attribute': referencedRelationship})
+    if intid is not None:
+        return catalog.findRelations({
+            'to_id': intid,
+            'from_attribute': referencedRelationship})
+    else:
+        intids = getUtility(IIntIds)
+        return catalog.findRelations({
+            'to_id': intids.getId(obj),
+            'from_attribute': referencedRelationship})
 
 
-def hasIncomingLinks(obj):
+def hasIncomingLinks(obj=None, intid=None):
     """Test if an object is linked to by other objects using
     plone.app.linkintegrity (Links in Richtext-Fields).
 
     Way to give bool without loading generator into list
     """
-    for i in getIncomingLinks(obj):
+    for i in getIncomingLinks(obj=obj, intid=intid):
         return True
     return False
 
 
-def getOutgoingLinks(obj):
+def getOutgoingLinks(obj=None, intid=None):
     """Return a generator of outgoing relations created using
     plone.app.linkintegrity (Links in Richtext-Fields).
     """
     catalog = getUtility(ICatalog)
-    intids = getUtility(IIntIds)
-    return catalog.findRelations({
-        'from_id': intids.getId(obj),
-        'from_attribute': referencedRelationship})
+    if intid is not None:
+        return catalog.findRelations({
+            'from_id': intid,
+            'from_attribute': referencedRelationship})
+    else:
+        intids = getUtility(IIntIds)
+        return catalog.findRelations({
+            'from_id': intids.getId(obj),
+            'from_attribute': referencedRelationship})
 
 
-def hasOutgoingLinks(obj):
+def hasOutgoingLinks(obj=None, intid=None):
     """Test if an object links to other objects using plone.app.linkintegrity
     (Links in Richtext-Fields).
     """
-    for i in getOutgoingLinks(obj):
+    for i in getOutgoingLinks(obj=obj, intid=intid):
         return True
     return False
 
