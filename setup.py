83748a2d5f54a2cc7025e3a0fcf8a1abc11e0ce0
Maurits van Rees <maurits@vanrees.org>
2018-04-04 17:34:00 +0200

    Updated classifiers: Plone 5.1 and 5.2.
    [ci skip]

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setup(
         "Environment :: Web Environment",
         "Framework :: Plone",
         "Framework :: Plone :: 5.1",
+        "Framework :: Plone :: 5.2",
         "Framework :: Zope2",
         "Intended Audience :: Other Audience",
         "Intended Audience :: System Administrators",
