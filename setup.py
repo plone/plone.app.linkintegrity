7d57d7e911d0cdeb2dfc8d271cb73ae6dc617093
Maurits van Rees <maurits@vanrees.org>
2020-03-28 01:23:10 +0100

    Added Python 3.8 to classifiers in setup.py.
    [ci skip]

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Internet :: WWW/HTTP :: Site Management :: Link Checking",
     ],
     keywords='link integrity plone',
